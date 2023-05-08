46a1f8ff19b09110075cb25877b8517a42d31f6f
Isaac True <isaac@is.having.coffee>
2023-05-08 15:54:17 +0200

    Initial commit
    Signed-off-by: Isaac True <isaac@is.having.coffee>

---
+++ src/main.c
@@ -0,0 +1,112 @@
+#include <android/sparse/sparse.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <assert.h>
+
+static int open_file(const char *filename, struct sparse_file **sparse);
+static void close_file(int fd, struct sparse_file *sparse);
+static void print_info(struct sparse_file *sparse);
+static int chunk_cb(void *priv, const void *data, size_t len);
+
+static void print_info(struct sparse_file *sparse)
+{
+	assert(sparse);
+
+	fprintf(stderr, "Sparse file size:    \t%.2f MiB\n",
+		sparse_file_len(sparse, true, true) / 1024.0 / 1024.0);
+	fprintf(stderr, "Extracted image size:\t%.2f MiB\n",
+		sparse_file_len(sparse, false, true) / 1024.0 / 1024.0);
+	fprintf(stderr, "Block size:          \t%d\n",
+		sparse_file_block_size(sparse));
+}
+
+static int open_file(const char *filename, struct sparse_file **sparse)
+{
+	int fd = 0;
+	struct sparse_file *_sparse;
+
+	assert(filename);
+	assert(sparse);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0) {
+		return -errno;
+	}
+
+	_sparse = sparse_file_import(fd, false, true);
+	if (!_sparse) {
+		fprintf(stderr, "Failed to open sparse file\n");
+		return -EIO;
+	}
+
+	*sparse = _sparse;
+
+	fprintf(stderr, "File opened: %s\n", filename);
+
+	return fd;
+}
+
+static void close_file(int fd, struct sparse_file *sparse)
+{
+	if (!sparse) {
+		sparse_file_destroy(sparse);
+	}
+	close(fd);
+}
+
+static int chunk_cb(void *priv, const void *data, size_t len)
+{
+	ssize_t ret = write(fileno(stdout), data, len);
+	if (ret == -1) {
+		fprintf(stderr, "Failed to write to stdout: %s\n", strerror(-errno));
+		return errno;
+	} else if ((size_t)ret != len) {
+		fprintf(stderr, "Could only write %ld bytes\n", ret);
+		return -1;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	struct sparse_file *sparse;
+	int fd;
+	int ret;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <sparse file>\n", argv[0]);
+		return -EINVAL;
+	}
+
+	if (isatty(fileno(stdout))) {
+		fprintf(stderr,
+			"Refusing to write to stdout! (Hint: pipe this to another program)\n");
+		return -EBADF;
+	}
+
+	fd = open_file(argv[1], &sparse);
+	if (fd < 0) {
+		fprintf(stderr, "Error opening sparse file: %s\n",
+			strerror(-fd));
+		return fd;
+	}
+
+	print_info(sparse);
+
+	ret = sparse_file_callback(sparse, false, false, chunk_cb, NULL);
+	if (ret < 0) {
+		fprintf(stderr, "Error reading sparse file: %s\n",
+			strerror(-fd));
+		return ret;
+	}
+
+	close_file(fd, sparse);
+
+	return 0;
+}
